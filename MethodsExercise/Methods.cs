aa14a2e2de69f763b8d3a16b73ad5c06dec3aa09
DavidRose <DavidRose331@Gmail.com>
2023-01-31 19:42:31 -0600

    Answer to Methods Excercise

---
+++ MethodsExercise/Methods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    public class Methods
+    {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+
+        }
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {userName}! Where do you go at night?");
+            var night = Console.ReadLine();
+
+            Console.WriteLine($"Thanks {userName}! what time do you come back?");
+            var time = Console.ReadLine();
+
+            Console.WriteLine($"Wow {userName}, why do you come back?!");
+            var come = Console.ReadLine();
+
+            Console.WriteLine($"Thanks, {userName}! Here is what you did lastnight");
+
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"night:{night}");
+            Console.WriteLine($"time: {time}");
+            Console.WriteLine($"come: {come}");
+
+            var answer = Methods.Sum(1, 1);
+            Console.WriteLine($"The answer is {answer}");
+
+            
+            var addAnswer = Methods.Sum(2, 4);
+            Console.WriteLine(addAnswer);
+
+            
+            var multiplyAnswer = Methods.Multiply(2, 10);
+            Console.WriteLine(multiplyAnswer);
+
+
+
         }
-    }
+    }               
 }
